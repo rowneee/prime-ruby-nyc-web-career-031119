c0727fddd8a2234cfd78d6e38752c7a507194d18
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 21:20:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
-  true
+  if integer >= 2
+    (2..integer - 1).all? do |x|
+      integer % x != 0
+    end
+  else
+    return false
+  end
 end
